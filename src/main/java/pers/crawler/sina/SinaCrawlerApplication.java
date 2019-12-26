43cc46e84a1d4009e1e493af68de3c0c963ac413
xiaoan <1214341420@qq.com>
2019-12-26 16:15:42 +0800

    目录创建

---
+++ src/main/java/pers/crawler/sina/SinaCrawlerApplication.java
@@ -0,0 +1,19 @@
+package pers.crawler.sina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: 新浪微博爬取应用启动类
+ * @author: Kangxiaoan
+ * @version:
+ * @date: 2019-12-25 14:56
+ */
+@SpringBootApplication
+public class SinaCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinaCrawlerApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/pers/crawler/sina/SinaCrawerApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.crawler.sina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinaCrawerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
